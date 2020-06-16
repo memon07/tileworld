3eb82d9e0a08cdb2e2103f6730d23189fba4a6e4
memon07 <memonshoyeb26@gmail.com>
2020-06-16 21:25:08 +0530

    character jump

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,13 +5,22 @@ using UnityStandardAssets.CrossPlatformInput;
 
 public class Player : MonoBehaviour
 {
+    // config
     [SerializeField] float runSpeed = 5f;
+    [SerializeField] float jumpSpeed = 28f;
+
+    //state
+    bool isAlive = true;
+
+    //cache
     Rigidbody2D myRigibody;
+    Animator myAnimator;
 
     // Start is called before the first frame update
     void Start()
     {
         myRigibody = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
         
     }
 
@@ -19,6 +28,7 @@ public class Player : MonoBehaviour
     void Update()
     {
         Run();
+        Jump();
         FlipSprite();
     }
 
@@ -27,6 +37,19 @@ public class Player : MonoBehaviour
         float controlThrow = CrossPlatformInputManager.GetAxis("Horizontal");  // -1 to +1
         Vector2 playerVelocity = new Vector2(controlThrow * runSpeed, myRigibody.velocity.y);
         myRigibody.velocity = playerVelocity;
+
+        // setting animation to running
+        bool playerHasHorizontalSpeed = Mathf.Abs(myRigibody.velocity.x) > Mathf.Epsilon;
+        myAnimator.SetBool("Running", playerHasHorizontalSpeed);
+    }
+
+    private void Jump()
+    {
+        if (CrossPlatformInputManager.GetButtonDown("Jump"))
+        {
+            Vector2 jumpVelocityToAdd = new Vector2(0f, jumpSpeed);
+            myRigibody.velocity += jumpVelocityToAdd;
+        }
     }
 
     private void FlipSprite()
