b4c58ceab8f2dd9719b1cc0f82bd36ee87a487ae
memon07 <memonshoyeb26@gmail.com>
2020-06-16 16:24:09 +0530

    player horizontal moment

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] float runSpeed = 5f;
+    Rigidbody2D myRigibody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigibody = GetComponent<Rigidbody2D>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Run();
+        FlipSprite();
+    }
+
+    private void Run()
+    {
+        float controlThrow = CrossPlatformInputManager.GetAxis("Horizontal");  // -1 to +1
+        Vector2 playerVelocity = new Vector2(controlThrow * runSpeed, myRigibody.velocity.y);
+        myRigibody.velocity = playerVelocity;
+    }
+
+    private void FlipSprite()
+    {
+        bool playerHasHorizontalSpeed = Mathf.Abs(myRigibody.velocity.x) > Mathf.Epsilon;
+        if (playerHasHorizontalSpeed)
+        {
+            transform.localScale = new Vector2(Mathf.Sign(myRigibody.velocity.x), 1f);
+        }
+    }
+}
