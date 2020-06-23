31f0134f8c1e290b48d4a8548372580522a25673
memon07 <memonshoyeb26@gmail.com>
2020-06-24 00:31:20 +0530

    enemy switching sides

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour
 {
+    [SerializeField] float moveSpeed = 1f;
+    Rigidbody2D myRigidbody2D;
+
     // Start is called before the first frame update
     void Start()
     {
+        myRigidbody2D = GetComponent<Rigidbody2D>();
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (IsFacingRight())
+        {
+            myRigidbody2D.velocity = new Vector2(moveSpeed, 0f);
+        }
+        else
+        {
+            myRigidbody2D.velocity = new Vector2(-moveSpeed, 0f);
+        }
+    }
+
+    bool IsFacingRight()
+    {
+        return transform.localScale.x > 0;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        transform.localScale = new Vector2(-(Mathf.Sign(myRigidbody2D.velocity.x)),1f);
     }
 }
